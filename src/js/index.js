77f9853cf8b3bb5d4e7de18cd98da8ba26075a25
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-01-05 22:55:35 +0200

    first 5 tasks

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// 1
+const age = 16;
+console.log(age);
+
+// 2
+const name = "Uliana";
+console.log(name);
+
+// 3
+const isStudent = true;
+console.log(isStudent);
+
+// 4
+const myString = "IGNITE - I grieve nothing I take everything. by Aaron Warner";
+console.log(myString);
+
+// 5
+let myNumber = 2878;
+myNumber = myNumber + 10;
+console.log(myNumber);
