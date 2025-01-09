80eb8a829384ec7d3ee14b0f2f8f398a19714c05
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-01-09 22:35:26 +0200

    hw is done now

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@ const age = 16;
 console.log(age);
 
 // 2
-const name = "Uliana";
-console.log(name);
+const nome = "Uliana";
+console.log(nome);
 
 // 3
 const isStudent = true;
@@ -20,3 +20,28 @@ console.log(myString);
 let myNumber = 2878;
 myNumber = myNumber + 10;
 console.log(myNumber);
+
+//6
+const myNull = null;
+console.log(myNull);
+
+//7
+const userName = prompt("Введіть своє ім'я");
+console.log("Користувачу", userName, "вітаємо Вас на цьому сайті!");
+
+//8
+const questionToCustomer = confirm("Чи хочете ви продовжити цю дію?");
+if (questionToCustomer) {
+  console.log("Дякую за підтвердження!");
+} else {
+  console.log("Дію відмінено!");
+}
+
+//9
+alert("Ця дію є небезпечною!");
+const askIfContinue = confirm("Підтвердити дію?");
+if (askIfContinue) {
+  console.log("Дякую за Ваше підтвердження!");
+} else {
+  console.log("Цю дію відмінено!");
+}
